0b9b3ba35710276a57b4550e15dd2370b86aa08b
tombryson <thomas.bryson@outlook.com>
2023-09-21 13:36:31 +1000

    css changes to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -17,68 +17,75 @@ const NavBar = () => {
   return (
     <div className="nav-bar">
       <div className="buttons-nav">
-        <ul>
-          <li className="icon-div">
-            <Link to="/">
-              <img
-                className="nav-icon"
-                src={iconHome}
-                alt="a house"
-                width="50px"
-              />
-            </Link>
-            <div className="icon-text">Home</div>
+        <ul className="nav-bar-icons">
+          <li className="nav-bar-icons">
+            <div className="icon-text">
+              Home
+              <Link to="/">
+                <img
+                  className="nav-icon"
+                  src={iconHome}
+                  alt="a house"
+                  width="50px"
+                />
+              </Link>
+            </div>
           </li>
           {currentUserId ? (
             <>
-              <li className="icon-div" onClick={logOut}>
-                <img
-                  className="nav-icon"
-                  src={myProfile}
-                  alt="a portrait"
-                  width="50px"
-                />
-                <div className="icon-text" id="log-out">
-                  Log out
+              <li className="nav-bar-icons">
+                <div className="icon-text" id="log-out" onClick={logOut}>
+                  Log Out
+                  <Link to="/">
+                    <img
+                      className="nav-icon"
+                      src={myProfile}
+                      alt="a portrait"
+                      width="50px"
+                    />
+                  </Link>
                 </div>
               </li>
-              <li className="icon-div">
-                <Link to="/book">
-                  <img
-                    className="nav-icon"
-                    src={book2}
-                    alt="a plane"
-                    width="50px"
-                  />
-                </Link>
-                <div className="icon-text" id="book">
+              <li className="nav-bar-icons">
+                <div className="icon-text">
                   Book
+                  <Link to="/book">
+                    <img
+                      className="nav-icon"
+                      src={book2}
+                      alt="a plane"
+                      width="50px"
+                    />
+                  </Link>
                 </div>
               </li>
-              <li className="icon-div">
-                <Link to="/mytrips">
-                  <img
-                    className="nav-icon"
-                    src={myTrips}
-                    alt="luggage"
-                    width="50px"
-                  />
-                </Link>
+              <li className="nav-bar-icons">
                 <div className="icon-text" id="my-trips">
-                  My Trips
+                  Trips
+                  <Link to="/mytrips">
+                    <img
+                      className="nav-icon"
+                      src={myTrips}
+                      alt="luggage"
+                      width="50px"
+                    />
+                  </Link>
                 </div>
               </li>
             </>
           ) : (
-            <li className="icon-div">
-              <Link to="/auth">
-                <img
-                  className="nav-icon"
-                  src={myProfile}
-                  alt="a portrait"
-                  width="50px"
-                />
-              </Link>
+            <li className="nav-bar-icons">
+              <div className="icon-text">
+                Sign In
+                <Link to="/auth">
+                  <img
+                    className="nav-icon"
+                    src={myProfile}
+                    alt="a portrait"
+                    width="50px"
+                  />
+                </Link>
+              </div>
             </li>
           )}
         </ul>
