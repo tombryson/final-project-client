78e07228bff04defa9b9328c430747ed3f7b5954
tombryson <thomas.bryson@outlook.com>
2023-10-16 18:01:23 +1100

    changes to header Nav and header styles

--- src/components/SiteHead.js
+++ src/components/SiteHead.js
@@ -5,6 +5,39 @@ import { Outlet, useNavigate } from 'react-router-dom';
 const SiteHead = () => {
   const navigate = useNavigate();
   const currentUserId = sessionStorage.getItem('currentUserId');
+  const [directions, setDirections] = useState({});
+
+  const handleMouseEnter = (e, id) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    let dir;
+
+    if (e.clientX - rect.left < rect.width / 2) {
+      dir = 'left-to-right';
+    } else {
+      dir = 'right-to-left';
+    }
+
+    setDirections((prevDirections) => ({
+      ...prevDirections,
+      [id]: dir,
+    }));
+  };
+
+  const handleMouseLeave = (e, id) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    let dir;
+
+    if (e.clientX - rect.left < rect.width / 2) {
+      dir = 'left-to-right';
+    } else {
+      dir = 'right-to-left';
+    }
+
+    setDirections((prevDirections) => ({
+      ...prevDirections,
+      [id]: dir,
+    }));
+  };
 
   const logOut = () => {
     sessionStorage.removeItem('currentUserId');
@@ -22,16 +55,34 @@ const SiteHead = () => {
         />
       </div>
       <div className="button-container">
-        <button>Home</button>
+        <button
+          className={`button ${directions['home']}`}
+          onMouseOver={(e) => handleMouseEnter(e, 'home')}
+          onMouseOut={(e) => handleMouseLeave(e, 'home')}
+        >
+          Home
+        </button>
         {currentUserId ? (
           <>
             <button>Sign Out</button>
             <button>My Trips</button>
           </>
         ) : (
-          <button>Sign In</button>
+          <button
+            className={`button ${directions['sign-in']}`}
+            onMouseOver={(e) => handleMouseEnter(e, 'sign-in')}
+            onMouseOut={(e) => handleMouseLeave(e, 'sign-in')}
+          >
+            Sign In
+          </button>
         )}
-        <button>Contact</button>
+        <button
+          className={`button ${directions['contact']}`}
+          onMouseOver={(e) => handleMouseEnter(e, 'contact')}
+          onMouseOut={(e) => handleMouseLeave(e, 'contact')}
+        >
+          Contact
+        </button>
       </div>
     </div>
   );
