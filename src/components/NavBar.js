686d1e1136487b7d519bb727a1fbef628cb3719d
tombryson <thomas.bryson@outlook.com>
2023-10-23 21:25:07 +1100

    css nav-bar text rotations

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,7 @@ import iconHome from '../images/home-icon.png';
 import myTrips from '../images/trips-icon.png';
 import myProfile from '../images/profile-icon.png';
 import book2 from '../images/book-icon2.png';
+import textRotations from './textRotations.css';
 
 const NavBar = () => {
   const navigate = useNavigate();
@@ -19,17 +20,20 @@ const NavBar = () => {
       <div className="buttons-nav">
         <ul className="nav-bar-icons">
           <li className="nav-bar-icons">
-            <div className="icon-text">
-              Home
-              <Link to="/">
-                <img
-                  className="nav-icon"
-                  src={iconHome}
-                  alt="a house"
-                  width="50px"
-                />
-              </Link>
-            </div>
+            <Link to="/">
+              <div className="icon-text">
+                <span>H</span>
+                <span>o</span>
+                <span>m</span>
+                <span>e</span>
+              </div>
+              <img
+                className="nav-icon"
+                src={iconHome}
+                alt="a house"
+                width="50px"
+              />
+            </Link>
           </li>
           {currentUserId ? (
             <>
@@ -75,17 +79,23 @@ const NavBar = () => {
             </>
           ) : (
             <li className="nav-bar-icons">
-              <div className="icon-text">
-                Sign In
-                <Link to="/auth">
-                  <img
-                    className="nav-icon"
-                    src={myProfile}
-                    alt="a portrait"
-                    width="50px"
-                  />
-                </Link>
-              </div>
+              <Link to="/auth">
+                <div className="icon-text">
+                  <span>S</span>
+                  <span>i</span>
+                  <span>h</span>
+                  <span>n</span>
+                  <span> </span>
+                  <span>i</span>
+                  <span>n</span>
+                </div>
+                <img
+                  className="nav-icon"
+                  src={myProfile}
+                  alt="a portrait"
+                  width="50px"
+                />
+              </Link>
             </li>
           )}
         </ul>
