89bba7f89dfa6bc0f14831d2fe81efdc6674be79
tombryson <thomas.bryson@outlook.com>
2024-06-27 21:04:02 +1000

    randomized video on page load

--- src/components/videoBackground.jsx
+++ src/components/videoBackground.jsx
@@ -1,15 +1,34 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 const VideoBackground = () => {
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const videos = [
+    require('../video/snow2-male-skier-skiing-downhill.webm'),
+    require('../video/snow3-slow-motion-cinematic-ski-slopes.webm'),
+    require('../video/beach1-tropical-ocean.webm'),
+  ];
+
+  useEffect(() => {
+    function randomiseVideo(videos) {
+      setSelectedVideo(videos[Math.floor(Math.random() * videos.length)]);
+    }
+
+    randomiseVideo(videos);
+  }, [videos]);
+
   return (
-    <video autoPlay muted loop className="background-video">
-      <source
-        src={require('../video/snow2-male-skier-skiing-downhill.webm')}
-        type="video/webm"
-      />
-      Your browser does not support the video tag.
-    </video>
+    <div>
+      {selectedVideo && (
+        <video autoPlay muted loop className="background-video">
+          <source src={selectedVideo} type="video/webm" />
+          Your browser does not support the video tag.
+        </video>
+      )}
+    </div>
   );
 };
 
