6d87569ffc64297343214ccf71694d5942429826
tombryson <thomas.bryson@outlook.com>
2024-07-05 23:46:26 +1000

    added adaptive backgrounds and shades to flight cards

--- src/components/ApiFlightTable.js
+++ src/components/ApiFlightTable.js
@@ -2,6 +2,7 @@ import { faCropSimple } from '@fortawesome/free-solid-svg-icons';
 import React, { useEffect, useState } from 'react';
 import qantas from '../images/qantas.jpeg';
 import jetstar from '../images/jetstar.png';
+import airnewzealand from '../images/airnewzealand.png';
 import unknown from '../images/unknown-carrier.png';
 import moment from 'moment';
 
@@ -10,10 +11,40 @@ const ApiFlightTable = ({ flights }) => {
     return moment(time24, 'HH:mm').format('h:mm A');
   }
 
+  const getBackgroundStyle = (code) => {
+    const backgroundStyles = {
+      QF: `linear-gradient(8deg, transparent 15px, #08090a 46px), 
+           linear-gradient(#cccccc 45%, #f6bbbb9c 26%, #830808f7 70%)`,
+      JQ: `linear-gradient(8deg, transparent 15px, rgb(8, 9, 10) 46px), 
+          linear-gradient(rgb(204, 204, 204) 45%, rgba(246, 187, 187, 0.61) 26%, rgb(229 79 15 / 80%) 70%)`,
+      ANZ: `linear-gradient(8deg, transparent 15px, rgb(8, 9, 10) 46px), linear-gradient(rgb(204, 204, 204) 45%, 
+          rgba(246, 187, 187, 0.61) 26%, rgb(161 161 161 / 80%) 70%)`,
+    };
+
+    const style =
+      backgroundStyles[code] ||
+      `linear-gradient(8deg, transparent 15px, #08090a 46px), 
+           linear-gradient(#cccccc 45%, #f6bbbb9c 26%, #830808f7 70%)`;
+    console.log(code);
+    console.log(style);
+    return style;
+  };
+
+  const getBackgroundImage = (code) => {
+    const backgroundColors = {
+      QF: '#080808;',
+      JQ: '#131314',
+      ANZ: '#feffff',
+    };
+
+    return backgroundColors[code] || 'white';
+  };
+
   const getAirlineImage = (code) => {
     const airlineImages = {
       QF: qantas,
       JQ: jetstar,
+      ANZ: airnewzealand,
     };
 
     const image = airlineImages[code] || unknown;
@@ -32,15 +63,20 @@ const ApiFlightTable = ({ flights }) => {
           <tr
             className="returned-flights"
             key={index}
-            style={{ animation: animStr(index) }}
+            style={{
+              animation: animStr(index),
+              backgroundImage: getBackgroundStyle(flight.carrier.iata),
+            }}
           >
             <td>
               <img
                 src={getAirlineImage(flight.carrier.iata)}
-                // src={qantas}
                 alt={flight.carrier.iata}
                 width="50"
                 height="50"
+                style={{
+                  backgroundColor: getBackgroundImage(flight.carrier.iata),
+                }}
               />
             </td>
             <th>Flight</th>
